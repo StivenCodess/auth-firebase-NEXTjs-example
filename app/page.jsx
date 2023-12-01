06243ff73a3f7d56c1b4ed817db0af5b4cd53ca6
Tiven <vairtor@gmail.com>
2023-11-30 22:27:28 -0300

    add home

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+"use client";
+import { auth } from "@/lib/firebase-config";
+import { signOut } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+	const router = useRouter();
+
+	const handlerSignOut = async () => {
+		//Sign out with the Firebase client
+		await signOut(auth);
+
+		//Clear the cookies in the server
+		const response = await fetch("http://localhost:3000/api/signOut", {
+			method: "POST",
+		});
+
+		if (response.status === 200) {
+			router.push("/login");
+		}
+	};
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex min-h-screen flex-col items-center p-24">
 			<h1>HOME</h1>
+			<button onClick={handlerSignOut}>SignOut</button>
 		</main>
 	);
 }
